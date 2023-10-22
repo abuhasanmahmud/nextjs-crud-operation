2bc7b51fb66fe967911e35d38857a564ed76db28
abuhasanmahmud <mahmud.me420@gmail.com>
2023-10-22 22:13:27 +0600

    f

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,5 +1,19 @@
-import { getTopicById } from "@/app/action/p";
 import EditTopicForm from "@/components/EditTopicForm";
+const getTopicById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topic");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default async function EditTopic({ params }) {
   const { id } = params;
